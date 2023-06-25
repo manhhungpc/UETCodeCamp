4fa09193785c75375b4653c3af896570d5f61e3a
manhhungpc <phamhung2002@gmail.com>
2023-06-26 01:10:35 +0700

    fe and homework 2

--- todo-note-fe/index.js
+++ todo-note-fe/index.js
@@ -2,36 +2,55 @@ let data = "";
 document.addEventListener("DOMContentLoaded", () => {
     getNote();
     const input = document.getElementById("input-note");
+    const submitBtn = document.getElementById("submit");
     // console.log(input);
-    input.addEventListener("change", (e) => submit(e));
+
+    submitBtn.addEventListener("click", async () => {
+        await submit(input.value);
+    });
 });
 
 //2. viết hàm getNote() để lấy data và hiển thị
-function getNote() {}
-
-function submit(event) {
-    console.log(event.target.value);
-    const submitBtn = document.getElementById("submit");
-    submitBtn.addEventListener("click", async (e) => {
-        const response = await fetch("http://localhost:3000/notes", {
-            method: "POST",
-            body: JSON.stringify({
-                name: event.target.value,
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-
-        const todo = document.getElementById("todo");
-        const { notes } = await response.json();
-        console.log(notes);
-        for (let list of notes) {
-            var li = document.createElement("li");
-            li.innerHTML = `<p>${list.name}</p>`;
-            todo.appendChild(li);
-            // todo.appendChild(`<li>${list.name}</li>`);
-            // todo.innerHTML = `<li>${list.name}</li>`;
-        }
+async function getNote() {
+    const response = await fetch("http://localhost:3000/notes", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
     });
+
+    const noteList = await response.json();
+    console.log(noteList);
+
+    const todo = document.getElementById("todo");
+    for (let note of noteList) {
+        let li = document.createElement("li");
+        li.innerHTML = `<p>${note.name}</p>`;
+        todo.appendChild(li);
+    }
+
+    // console.log(await response.json());
+}
+
+async function submit(data) {
+    const response = await fetch("http://localhost:3000/notes", {
+        method: "POST",
+        body: JSON.stringify({
+            name: data,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    // {notes: [...]}
+    const { notes } = await response.json();
+    // console.log(notes);
+
+    const todo = document.getElementById("todo");
+    todo.innerHTML = "";
+    for (let list of notes) {
+        let li = document.createElement("li");
+        li.innerHTML = `<p>${list.name}</p>`;
+        todo.appendChild(li);
+    }
 }
